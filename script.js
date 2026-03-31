3764fb78d5d4b7b880c55211b3400408f714ca6d
andre luiz martins santiago <santiiagoo2405@gmail.com>
2026-03-31 17:37:11 -0300

    quase 100%
    ajustes no css

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
-
 document.addEventListener(`DOMContentLoaded`, () => {
-    const artistsData = [
-      { name: 'Henrique & Juliano', image: './img/artista-henrique-juliano.jpeg' },
-      { name: 'Jorge & Mateus', image: './img/artista-jorge-mateus.jpeg' },
-      { name: 'Zé Neto & Cristiano', image: './img/artista-ze-neto.jpeg' },
-      { name: 'Gusttavo Lima', image: './img/artista-gustavo-limma.jpeg' },
-      { name: 'Luan Santana', image: './img/artista-luan-santana.jpeg' },
-      { name: 'Matheus & Kauan', image: './img/artista-mateus-kauan.jpeg' }
-    ];
-    
-    const albumsData = [
-      { name: 'White Noise (Sleep & Relaxation Sounds)', artist: 'Sleepy John', image: './img/album-white-noise.jpeg' },
-      { name: 'O Céu Explica Tudo (Ao Vivo)', artist: 'Henrique & Juliano', image: './img/album-ceu-explica-tudo.jpeg' },
-      { name: 'Nada como um dia após o outro', artist: 'Racionais', image: './img/album-vida-loka.jpeg' },
-      { name: 'HIT ME HARD AND SOFT', artist: 'Billie Eilish', image: './img/album-hit-me.jpeg' },
-      { name: 'CAJU', artist: 'Liniker', image: './img/album-caju.jpeg' },
-      { name: 'Escândalo Íntimo', artist: 'Luísa Sonza', image: './img/album-escandalo.jpeg' }
-    ];
+  const artistsData = [
+    { name: "Henrique & Juliano", image: "./img/artista-henrique-juliano.jpg" },
+    { name: "Jorge & Mateus", image: "./img/artista-jorge-mateus.jpg" },
+    { name: "Zé Neto & Cristiano", image: "./img/artista-ze-neto.jpg" },
+    { name: "Gusttavo Lima", image: "./img/artista-gustavo-limma.jpg" },
+    { name: "Luan Santana", image: "./img/artista-luan-santana.jpg" },
+    { name: "Matheus & Kauan", image: "./img/artista-mateus-kauan.jpg" },
+  ];
+
+  const albumsData = [
+    {
+      name: "White Noise (Sleep & Relaxation Sounds)",
+      artist: "Sleepy John",
+      image: "./img/album-white-noise.jpg",
+    },
+    {
+      name: "O Céu Explica Tudo (Ao Vivo)",
+      artist: "Henrique & Juliano",
+      image: "./img/album-ceu-explica.jpg",
+    },
+    {
+      name: "Nada como um dia após o outro",
+      artist: "Racionais",
+      image: "./img/album-vida-loka.jpg",
+    },
+    {
+      name: "HIT ME HARD AND SOFT",
+      artist: "Billie Eilish",
+      image: "./img/album-hit-me.jpg",
+    },
+    { name: "CAJU", artist: "Liniker", image: "./img/album-caju.jpg" },
+    {
+      name: "Escândalo Íntimo",
+      artist: "Luísa Sonza",
+      image: "./img/album-escandalo.jpg",
+    },
+  ];
+
+  const artistgrid = document.querySelector(`.artists-grid`);
+  const albumgrid = document.querySelector(`.albums-grid`);
+
+  artistsData.forEach((artist) => {
+    const artistCard = document.createElement(`div`);
+    artistCard.classList.add(`artist-card`);
+
+    artistCard.innerHTML = `
+
+        <img src="${artist.image}" alt="${artist.name}">
+          <div>
+            <h3>${artist.name}</h3>
+            <p>Artista</p>
+          </div>
 
 
-    const artistgrid = document.querySelector(`.artists-grid`)
-    const albumgrid = document.querySelector(`.albums-grid`)
+        `;
 
-    artistsData.forEach( artist => {
-        const artistCard = document.createElement(`div`)
-        artistCard.classList.add(`artist-card`)
+    artistgrid.appendChild(artistCard);
+  });
 
-        artistCard.innerHTML = `
+  albumsData.forEach((albums) => {
+    const albumCard = document.createElement(`div`);
+    albumCard.classList.add(`album-card`);
 
-          <img src="${artist.image}" alt="${artist.name}">
-          <h3>${artist.name}<h3>
-          <p>artista<p
+    albumCard.innerHTML = `
+          
+        <img src="${albums.image}" alt="${albums.name}">
+          <div>
+            <h3>${albums.name}</h3>
+            <p>${albums.artist}</p>
+          </div>
 
-
-        `
-
-        artistgrid.app
-    })
-
-})
-
+      `;
+      albumgrid.appendChild(albumCard)
+  });
+});
